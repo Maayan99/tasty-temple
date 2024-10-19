e174da1e59ac9b60117bd373fa5b07eaf6a9adc6
Maayan99 <113969733+Maayan99@users.noreply.github.com>
2024-10-20 01:14:47 +0300

    || ""

--- src/components/BlogContent.tsx
+++ src/components/BlogContent.tsx
@@ -75,7 +75,7 @@ const BlogContent: React.FC<BlogContentProps> = ({ content, images }) => {
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ delay: index * 0.1, duration: 0.3 }}
               >
-                {formatContent(item.content)}
+                {formatContent(item.content || "")}
               </MotionTag>
             );
           } else if (item.type === 'paragraph') {
