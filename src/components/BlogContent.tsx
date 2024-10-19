9d2b4eb560f568ae358d3c554512be5e2ac71c83
Maayan99 <113969733+Maayan99@users.noreply.github.com>
2024-10-19 17:20:26 +0300

    ts hoops

--- src/components/BlogContent.tsx
+++ src/components/BlogContent.tsx
@@ -60,7 +60,7 @@ const BlogContent: React.FC<BlogContentProps> = ({ content, images }) => {
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ delay: index * 0.1, duration: 0.3 }}
               >
-                {item.content}
+                {"content" in item && item.content}
               </motion.h3>
             );
           } else if (item.type === 'paragraph') {
@@ -72,7 +72,7 @@ const BlogContent: React.FC<BlogContentProps> = ({ content, images }) => {
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ delay: index * 0.1, duration: 0.3 }}
               >
-                {item.content}
+                {"content" in item && item.content}
               </motion.p>
             );
           } else if (item.type === 'image') {
