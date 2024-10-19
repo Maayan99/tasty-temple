00ef7c0d47d196c3ef8b4f1f18373ddccc11fef6
Maayan99 <113969733+Maayan99@users.noreply.github.com>
2024-10-19 20:36:09 +0300

    trying something else, perhaps

--- src/app/api/generate-recipes/start/route.ts
+++ src/app/api/generate-recipes/start/route.ts
@@ -27,7 +27,7 @@ export async function POST(request: Request) {
     const recipeIdeas = JSON.parse(cleanedIdeasJson);
 
 
-    console.log("Calling next stop, ", `${process.env.NEXT_PUBLIC_API_URL}/api/generate-recipes/generate-full-recipe`)
+    console.log("Calling next stop, ", `/api/generate-recipes/generate-full-recipe`)
     // Call the next step in the process
     fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/generate-recipes/generate-full-recipe`, {
       method: 'POST',
