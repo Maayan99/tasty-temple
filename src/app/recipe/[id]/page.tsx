2e606e4b3628e52ca3b586c4a4d8de882939f664
Maayan99 <113969733+Maayan99@users.noreply.github.com>
2024-10-19 14:57:06 +0300

    by slug

--- src/app/recipe/[id]/page.tsx
+++ src/app/recipe/[id]/page.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { notFound } from 'next/navigation';
 import RecipeContent from '@/components/RecipeContent';
-import { getRecipeById } from '@/lib/recipes';
+import { getRecipeBySlug } from '@/lib/recipes';
 
 export default async function RecipePage({ params }: { params: { id: string } }) {
-  const recipe = await getRecipeById(parseInt(params.id));
+  const recipe = await getRecipeBySlug(parseInt(params.id));
 
   if (!recipe) {
     notFound();
